118d66a7d9c13197b72230121f163f51ec559a7b
Walter Hoerman <whoerman@gmail.com>
2020-12-23 10:39:37 -0500

    Create intitial React App

---
+++ bhtnh/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {  return (
+    <div className="App">
+      <header className="App-header">
+          Black Heritage Trail of NH
+      </header>
+    </div>
+  );
+}
+
+export default App;
