1e00091a801330abe0d3a80ad9a40c5b54e0ad17
Walter Hoerman <whoerman@gmail.com>
2020-12-23 12:33:49 -0500

    starting site array

---
+++ bhtnh/src/siteArray.js
@@ -0,0 +1,14 @@
+//array of black heritage trail sites
+let siteArray = [
+    {
+    number: 1,
+    name: 'Black Heritage Trail of New Hampshire office',
+    address: '222 Court Street'
+},
+{
+    number: 2,
+    name: 'Governor John Langdon House',
+    address: '143 Pleasant Street'
+},
+
+]
